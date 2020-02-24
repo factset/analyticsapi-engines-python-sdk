e6dd145c1f2a87c824c31356cbdbf20b747c0986
Sai Giridhar <spasumarthi@factset.com>
2020-02-24 14:16:59 +0530

    fix(codegen): Fixing the url of openapi-generator-url (#8)

--- Utilities/codegen/download-codegen.bat
+++ Utilities/codegen/download-codegen.bat
@@ -4,6 +4,6 @@ SETLOCAL
 
 SET script-path=%~dp0
 
-powershell -Command "(new-object System.Net.WebClient).DownloadFile('http://central.maven.org/maven2/org/openapitools/openapi-generator-cli/4.2.2/openapi-generator-cli-4.2.2.jar', '%script-path%openapi-generator-cli-4.2.2.jar')"
+powershell -Command "(new-object System.Net.WebClient).DownloadFile('http://insecure.repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/4.2.2/openapi-generator-cli-4.2.2.jar', '%script-path%openapi-generator-cli-4.2.2.jar')"
 
 ENDLOCAL
