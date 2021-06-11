9cf8f668eda2c0175ea63d6a55427a69f4576568
Hank <hank.lin@factset.com>
2021-06-11 15:49:45 -0400

    updated recursive call for api workflow runner

--- tests/test_pa_calculations_api.py
+++ tests/test_pa_calculations_api.py
@@ -104,11 +104,11 @@ class TestPaCalculationsApi(unittest.TestCase):
 
 def run_api_workflow_with_assertions(workflow_specification, current_request, test_context):
     current_request_result = current_request(test_context)
-    if current_request_result["continue_workflow"]:
+    if current_request_result is not None and current_request_result["continue_workflow"]:
         run_api_workflow_with_assertions(
             workflow_specification,
-            current_request_result[current_request_result["next_request"]],
-            current_request_result["test_context"]
+            workflow_specification[current_request_result["next_request"]],
+            test_context
         )
 
 
