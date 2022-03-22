d6ab097b3e1fb0df3cb049f6074cbca6fa3c4207
Kiranmai Sreeramoju <kiranmai.sreeramoju@factset.com>
2022-03-22 11:45:43 +0530

    minor fix

--- tests/test_discountcurves_api.py
+++ tests/test_discountcurves_api.py
@@ -21,8 +21,7 @@ class TestDiscountCurvesApi(unittest.TestCase):
             len(response[0].data), 0, "Response result should not be an empty list.")
         
     def test_get_all_discount_curves_currency_usd(self):
-        self.currency = "USD"
-        response = self.discount_curves_api.get_all_fi_discount_curves()
+        response = self.discount_curves_api.get_all_fi_discount_curves(currency = "INR")
         currency_code = list(response[0].data.keys())[0]
         self.assertEqual(response[1], 200, "Response should be 200 - Success")
         self.assertEqual(type(response[0].data), dict,
@@ -31,11 +30,9 @@ class TestDiscountCurvesApi(unittest.TestCase):
             len(response[0].data), 0, "Response result should not be an empty list.")
         
     def test_get_all_discount_curves_currency_randomvalue(self):
-        self.currency = "randomvalue"
-        response = self.discount_curves_api.get_all_fi_discount_curves()
+        response = self.discount_curves_api.get_all_fi_discount_curves(currency = "randomValue")
         currency_code = list(response[0].data.keys())[0]
         self.assertEqual(response[1], 404, "Response should be 404 - Not Found")
-
 
 if __name__ == '__main__':
     unittest.main()
